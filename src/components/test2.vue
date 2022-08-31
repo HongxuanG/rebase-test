48809cb6bf2cb3fe5fd1c4c9461944608740f255
HongxuanG <1359774872@qq.com>
2022-08-31 16:50:42 +0800

    ✨ feat(test2): 在test分支上提交test2的代码、

--- src/components/test2.vue
+++ src/components/test2.vue
@@ -2,6 +2,7 @@
   <div class="main">这是test分支的counter</div>
   <div class="main">这是test分支的counter</div>
   <div class="main">这是test分支的counter</div>
+  <p>现在我在test分支上提交代码commit</p>
 </template>
 
 <script setup></script>
