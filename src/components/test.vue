2034416e71f0bbf82cc6f8f9282a6dea1dc05697
HongxuanG <1359774872@qq.com>
2022-08-31 19:53:56 +0800

    feat: 合并提交

--- src/components/test.vue
+++ src/components/test.vue
@@ -7,6 +7,8 @@
   <div class="main">新增一个counter</div>
   <div class="main">新增一个counter</div>
   <p>现在我在main分支上提交代码（没有拉取test分支）</p>
+  <div>临时提交1</div>
+  <div>临时提交2</div>
 </template>
 
 <script setup></script>
