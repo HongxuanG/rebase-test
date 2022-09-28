b7f5b2feb131aae0596dc7a03dfd627893c2275b
HongxuanG <1359774872@qq.com>
2022-09-28 22:46:28 +0800

    🎈 perf(main): main分支提交 c2

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,9 @@ import { defineComponent, h } from "vue"
 export default defineComponent(() => {
   const vnode = h('div', {}, 'ssss')
   console.log('vnode==>', vnode)
+
+  const a = 11
+  console.log(a)
+  
   return vnode
 })
